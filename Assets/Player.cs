4416849131616bbaa275ada74b2f99332c152837
Ivan Teong <ivanteongsh@gmail.com>
2018-09-24 23:46:23 +1000

    Allowing ship to move and clamp movement so it won't go outside window screen

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,17 +5,30 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float speed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
-        print(xOffsetThisFrame); // this value of m per sec * FPS should give value close to xSpeed of 4f m per sec
-	}
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        // this value of m per sec * FPS should give value close to xSpeed of 4f m per sec
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampedNewXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float clampedNewYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedNewXPos, clampedNewYPos, transform.localPosition.z);
+    }
 }
