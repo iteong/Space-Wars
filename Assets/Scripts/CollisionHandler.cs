628017f5a4a752dca29de5bb2b16a37a9661b814
Ivan Teong <ivanteongsh@gmail.com>
2018-10-29 21:53:31 +1100

    Trigger explosion game object FX and reload scene upon collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,17 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // ok as long as this is the only script loading scenes
 
 public class CollisionHandler : MonoBehaviour {
+
+    // todo work out why sometimes slow on first play of scene
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("Fx prefab on player")][SerializeField] GameObject deathFx;
 
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFx.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
         print("Player dying");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene() {
+        SceneManager.LoadScene(1);
     }
 }
