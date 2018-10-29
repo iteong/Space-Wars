9e8832991a07e39f81773a899803ca662932ff8d
Ivan Teong <ivanteongsh@gmail.com>
2018-10-29 22:40:35 +1100

    Shoot enemy ships and destroy them

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -6,6 +6,11 @@ public class MusicPlayer : MonoBehaviour {
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayers > 1) {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
